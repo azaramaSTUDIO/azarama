aea775eff6436ca2d06a0bb3e667dd49da74dac2
WonZun Zo <banana36573@naver.com>
2018-04-16 16:01:35 +0900

    파이프 생성 코드
    파이프를 생성하는 코드를 만들었습니다.

--- Assets/02. Scripts/GameManager.cs
+++ Assets/02. Scripts/GameManager.cs
@@ -2,16 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
+    public Transform spawnPoint;
     public static GameManager instance;
+    public GameObject Pipes;
+    public float spawnTime = 3.0f;
 
     public float sfxVolume = 0.9f; // 효과음 음량
     public bool isSfxMute = false; // 효과음 음소거 여부 설정
 
-	// Use this for initialization
-	void Awake () {
-		
+    // Use this for initialization
+    void Awake()
+    {
+
         if (instance == null)
         {
             instance = this;
@@ -21,12 +26,18 @@ public class GameManager : MonoBehaviour {
             Destroy(gameObject);
         }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    }
+
+    void Start()
+    {
+        StartCoroutine(SpawnSphere());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void PlaySfx(Vector3 pos, AudioClip sfx)
     {
@@ -48,5 +59,14 @@ public class GameManager : MonoBehaviour {
         audioSource.Play();
 
         Destroy(soundObj, sfx.length);
+    }
+
+    IEnumerator SpawnSphere()
+    {
+        while (true)
+        {
+            GameObject obj1 = Instantiate(Pipes, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(spawnTime);
+        }
     }
 }
