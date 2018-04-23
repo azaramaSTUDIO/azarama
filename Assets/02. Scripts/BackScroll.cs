28168e1701b0086c1b48277ea4756bf583091863
WonZun Zo <banana36573@naver.com>
2018-04-23 11:01:14 +0900

    편집이 꼬였습니다.
    물리 엔진 등이 꼬이는 문제가 발생했습니다. 여진쌤의 도움이 필요합니다.

---
+++ Assets/02. Scripts/BackScroll.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackScroll : MonoBehaviour {
+
+    public float scrollSpeed = 1.5f;
+    Material myMaterial;
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+    }
+    void Update()
+    {
+        float newOffsetY = myMaterial.mainTextureOffset.y + scrollSpeed * Time.deltaTime;
+        Vector2 newOffset = new Vector2(0, newOffsetY);
+        myMaterial.mainTextureOffset = newOffset;
+    }
+}
