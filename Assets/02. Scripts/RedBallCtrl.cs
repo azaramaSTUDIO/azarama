ca6aad334787f3a1f7ea2ca1f1ce26ed2e9819c0
WonZun Zo <banana36573@naver.com>
2018-04-23 15:15:25 +0900

    레드볼 회전 수정
    레드볼이 가로와 세로로 회전하도록 했습니다.

--- Assets/02. Scripts/RedBallCtrl.cs
+++ Assets/02. Scripts/RedBallCtrl.cs
@@ -22,6 +22,6 @@ public class RedBallCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(new Vector3(0, 180, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(180, 180, 0) * Time.deltaTime);
     }
 }
