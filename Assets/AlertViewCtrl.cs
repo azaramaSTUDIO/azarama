b22cb0d97d8d42e0f7959f58e1cdd681d5ae1afa
Yuuki-onejae <38270006+Yuuki-onejae@users.noreply.github.com>
2018-05-09 14:47:04 +0900

    알림뷰 생성
    알림뷰 UI와 스크립트 일부를 작성했습니다

---
+++ Assets/AlertViewCtrl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertViewCtrl : MonoBehaviour {
+
+    public string noButtonTitle;
+    public System.Action noButtonDelegate;
+    public string yesButtonTitle;
+    public System.Action yesButtonDelegate;
+}
+	// Update is called once per frame
+	
+    [SerializeField]
+    [SerializeField]
+    
+
+    
+		
+	
